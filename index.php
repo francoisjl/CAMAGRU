90cc8f28510e1a98042e272d1e6f0918127b68b7
Franck DUPEYROUX <fdupeyro@e1r11p21.42.fr>
2017-03-28 14:47:03 +0200

    delete

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('includes_session.php');
-if (!$_SESSION["email"] || $_SESSION["valide"] !='ok') {header('Location: login.php');} 
-if ($_SESSION["valide"]=='ok') {header('Location: home.php');}
-?>
